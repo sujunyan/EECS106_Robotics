a4bb013f586a6bcf24dd4d7299ce82f1aa39e792
yzs15 <614140848@qq.com>
2018-09-14 00:10:10 +0800

    lab3 task1

---
+++ lab3/build/chatter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/albert/ros/lab3/Lab_3_Resources/lab3/src/chatter
+# Build directory: /home/albert/ros/lab3/Lab_3_Resources/lab3/build/chatter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
