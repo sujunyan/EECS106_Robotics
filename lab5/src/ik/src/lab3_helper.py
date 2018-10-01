25ea05ebb1f9cc3a95916594e0b98022924a9472
sujuanyan <sujy@shanghaitech.edu.cn>
2018-10-01 16:09:38 -0700

    update lab3_helper.py

--- lab5/src/ik/src/lab3_helper.py
+++ lab5/src/ik/src/lab3_helper.py
@@ -3,6 +3,162 @@
 import numpy as np
 import scipy as sp
 
+
+def skew_3d(omega):
+    """
+    Converts a rotation vector in 3D to its corresponding skew-symmetric matrix.
+
+    Args:
+    omega - (3,) ndarray: the rotation vector
+
+    Returns:
+    omega_hat - (3,3) ndarray: the corresponding skew symmetric matrix
+    """
+    if not omega.shape == (3,):
+        raise TypeError('omega must be a 3-vector')
+    omega_hat = np.array([[0, -omega[2], omega[1]],[omega[2], 0, -omega[0]], [-omega[1], omega[0], 0]])
+
+    #YOUR CODE HERE
+
+    return omega_hat
+
+def rotation_2d(theta):
+    """
+    Computes a 2D rotation matrix given the angle of rotation.
+
+    Args:
+    theta: the angle of rotation
+
+    Returns:
+    rot - (2,2) ndarray: the resulting rotation matrix
+    """
+
+    #YOUR CODE HERE
+    rot = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta), np.cos(theta)]])
+    return rot
+
+def rotation_3d(omega, theta):
+    """
+    Computes a 3D rotation matrix given a rotation axis and angle of rotation.
+
+    Args:
+    omega - (3,) ndarray: the axis of rotation
+    theta: the angle of rotation
+
+    Returns:
+    rot - (3,3) ndarray: the resulting rotation matrix
+    """
+    if not omega.shape == (3,):
+        raise TypeError('omega must be a 3-vector')
+
+    #YOUR CODE HERE
+    omega_hat = skew_3d(omega)
+    rot = linalg.expm(omega_hat*theta)
+    return rot
+
+def hat_2d(xi):
+    """
+    Converts a 2D twist to its corresponding 3x3 matrix representation
+
+    Args:
+    xi - (3,) ndarray: the 2D twist
+
+    Returns:
+    xi_hat - (3,3) ndarray: the resulting 3x3 matrix
+    """
+    if not xi.shape == (3,):
+        raise TypeError('omega must be a 3-vector')
+
+    #YOUR CODE HERE
+    xi_hat = np.array([[0, -xi[2], xi[0]], [xi[2], 0, xi[1]], [0,0,0]])
+    return xi_hat
+
+def hat_3d(xi):
+    """
+    Converts a 3D twist to its corresponding 4x4 matrix representation
+
+    Args:
+    xi - (6,) ndarray: the 3D twist
+
+    Returns:
+    xi_hat - (4,4) ndarray: the corresponding 4x4 matrix
+    """
+    if not xi.shape == (6,):
+        raise TypeError('xi must be a 6-vector')
+
+    #YOUR CODE HERE
+    xi_hat = np.zeros((4,4))
+    v = xi[0 : 3]
+    omega = xi[3 : 6]
+    omega_hat = skew_3d(omega)
+    xi_hat[0:3, 0:3] = omega_hat
+    #print(np.transpose(v))
+    xi_hat[0:3, 3] = np.transpose(v)
+    #print xi
+    #print xi_hat
+    return xi_hat
+
+def homog_2d(xi, theta):
+    """
+    Computes a 3x3 homogeneous transformation matrix given a 2D twist and a
+    joint displacement
+
+    Args:
+    xi - (3,) ndarray: the 2D twist
+    theta: the joint displacement
+
+    Returns:
+    g - (3,3) ndarray: the resulting homogeneous transformation matrix
+    """
+    if not xi.shape == (3,):
+        raise TypeError('xi must be a 3-vector')
+
+    #YOUR CODE HERE
+    xi_hat = hat_2d(xi)
+    g = linalg.expm(xi_hat*theta)
+    return g
+
+def homog_3d(xi, theta):
+    """
+    Computes a 4x4 homogeneous transformation matrix given a 3D twist and a
+    joint displacement.
+
+    Args:
+    xi - (6,) ndarray: the 3D twist
+    theta: the joint displacement
+
+    Returns:
+    g - (4,4) ndarary: the resulting homogeneous transformation matrix
+    """
+    if not xi.shape == (6,):
+        raise TypeError('xi must be a 6-vector')
+
+    #YOUR CODE HERE
+    xi_hat = hat_3d(xi)
+    g = linalg.expm(xi_hat*theta)
+    return g
+
+def prod_exp(xi, theta):
+    """
+    Computes the product of exponentials for a kinematic chain, given
+    the twists and displacements for each joint.
+
+    Args:
+    xi - (6,N) ndarray: the twists for each joint
+    theta - (N,) ndarray: the displacement of each joint
+
+    Returns:
+    g - (4,4) ndarray: the resulting homogeneous transformation matrix
+    """
+    if not xi.shape[0] == 6:
+        raise TypeError('xi must be a 6xN')
+
+    #YOUR CODE HERE
+    g = homog_3d(xi[0:6, 0],theta[0])
+    for i in range(1, xi.size / 6):
+        g =g.dot(homog_3d(xi[0:6, i],theta[i]))
+    return g
+
 def prod_exp(xi, theta):
     """
     Computes the product of exponentials for a kinematic chain, given
